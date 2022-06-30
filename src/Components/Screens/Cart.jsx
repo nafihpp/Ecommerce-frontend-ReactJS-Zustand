d1b8f5fbb7054849efa1617da0cf55c932063e17
nafihpp <nafihpp@gmail.com>
2022-06-30 12:19:42 +0530

    ecommerce

--- src/Components/Screens/Cart.jsx
+++ src/Components/Screens/Cart.jsx
@@ -1,138 +1,136 @@
-import React from 'react';
+import React from "react";
 import Helmet from "react-helmet";
 import styled from "styled-components";
 
-function Cart({item, setItem}) {
-  let cartitems = () => {
-    return item.map((cartss) => (
-      <>
-       <CartItems key={cartss.id}>
-          <ImageBox>
-            <Image src={cartss.image} />
-          </ImageBox>
-          <AboutDiv>
-            <Title>{cartss.title}</Title>
-            <SubTitle>{cartss.category}</SubTitle>
-          </AboutDiv>
-          <PricesDiv>
-            <DivAmount>${cartss.price}</DivAmount>
-            <DivRemove>
-              <u>Remove</u>
-            </DivRemove>
-          </PricesDiv>
-        </CartItems> 
-      </>
-    ));
-  };
-  return (
-    <Mainof>
-      <HeadDivision>
-        <MainHead>
-          <Helmet>
-            <title>My Cart</title>
-          </Helmet>
-          <Heading>Shopping Cart</Heading>
-          <Removeall>Remove all</Removeall>
-        </MainHead>
-        {cartitems()}
-        <HorizontalLine />
-        <h1>Total:
-        </h1>
-      </HeadDivision>
-    </Mainof>
-  );
+function Cart({ item, setItem }) {
+    let cartitems = () => {
+        return item.map((cartss) => (
+            <>
+                <CartItems key={cartss.id}>
+                    <ImageBox>
+                        <Image src={cartss.image} />
+                    </ImageBox>
+                    <AboutDiv>
+                        <Title>{cartss.title}</Title>
+                        <SubTitle>{cartss.category}</SubTitle>
+                    </AboutDiv>
+                    <PricesDiv>
+                        <DivAmount>${cartss.price}</DivAmount>
+                        <DivRemove>
+                            <u>Remove</u>
+                        </DivRemove>
+                    </PricesDiv>
+                </CartItems>
+            </>
+        ));
+    };
+    return (
+        <Mainof>
+            <HeadDivision>
+                <MainHead>
+                    <Helmet>
+                        <title>My Cart</title>
+                    </Helmet>
+                    <Heading>Shopping Cart</Heading>
+                    <Removeall>Remove all</Removeall>
+                </MainHead>
+                {cartitems()}
+                <HorizontalLine />
+                <h1>Total:</h1>
+            </HeadDivision>
+        </Mainof>
+    );
 }
 const DivAmount = styled.div`
-  font-size: 26px;
-  font-family: "Open Sans";
-  font-weight: 800;
-  color: #202020;
+    font-size: 26px;
+    font-family: "Open Sans";
+    font-weight: 800;
+    color: #202020;
 `;
 const DivRemove = styled.div`
-  padding-top: 5px;
-  font-size: 14px;
-  font-family: "Open Sans";
-  font-weight: 600;
-  color: #e44c4c;
-  cursor: pointer;
+    padding-top: 5px;
+    font-size: 14px;
+    font-family: "Open Sans";
+    font-weight: 600;
+    color: #e44c4c;
+    cursor: pointer;
 `;
 const PricesDiv = styled.div`
-  height: 100%;
-  text-align: right;
+    height: 100%;
+    text-align: right;
 `;
 const SubTitle = styled.h3`
-  font-size: 18px;
-  font-family: "Open Sans";
-  font-weight: 600;
-  color: #909090;
+    font-size: 18px;
+    font-family: "Open Sans";
+    font-weight: 600;
+    color: #909090;
 `;
 const Title = styled.h1`
-  padding-top: 10px;
-  font-size: 14px;
-  font-family: "Open Sans";
-  font-weight: 800;
-  color: #202020;
+    padding-top: 10px;
+    font-size: 14px;
+    font-family: "Open Sans";
+    font-weight: 800;
+    color: #202020;
 `;
 const AboutDiv = styled.div`
-  height: 100%;
-  width: 24%;
+    height: 100%;
+    width: 24%;
 `;
 const Image = styled.img`
-  display: block;
-  width: 100%;
-  height: 120px;
+    display: block;
+    width: 100%;
 `;
 const ImageBox = styled.div`
-  width: 15%;
-  text-align: center;
+    width: 15%;
+    text-align: center;
 `;
 const CartItems = styled.div`
-  margin: 40px auto;
-  width: 90%;
-  height: 30%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    margin: 40px auto;
+    width: 90%;
+    height: 30%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 const Removeall = styled.h5``;
 const Heading = styled.h3`
-  font-size: 20px;
-  font-family: "Open Sans";
-  font-weight: 700;
-  color: #2f3841;
+    font-size: 20px;
+    font-family: "Open Sans";
+    font-weight: 700;
+    color: #2f3841;
 `;
 const HorizontalLine = styled.hr`
-  width: 66%;
-  float: right;
-  margin-right: 5%;
+    width: 66%;
+    float: right;
+    margin-right: 5%;
 `;
 const MainHead = styled.div`
-  margin: auto;
-  width: 91%;
-  height: 41%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding-top: 20px;
+    margin: auto;
+    width: 91%;
+    height: 41%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 20px;
 `;
 const Mainof = styled.section`
-  margin: 0;
-  padding: 0;
-  background: linear-gradient(to bottom right, #e3f0ff, #fafcff);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 50px 0;
+    margin: 0;
+    padding: 0;
+    background: linear-gradient(to bottom right, #e3f0ff, #fafcff);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 50px 0;
 `;
 const HeadDivision = styled.div`
-  width: 70%;
-  height: 90%;
-  background-color: #ffffff;
-  border-radius: 20px;
-  box-shadow: 0px 10px 20px #1687d933;
-  @media all and (max-width: 768px) {
-    width: 95%;
-  }
+    width: 70%;
+    height: 90%;
+    background-color: #ffffff;
+    border-radius: 20px;
+    box-shadow: 0px 10px 20px #1687d933;
+    @media all and (max-width: 768px) {
+        width: 95%;
+    }
 `;
 
 export default Cart;
--- src/Components/Screens/Items.jsx
+++ src/Components/Screens/Items.jsx
@@ -10,160 +10,162 @@ import Apricot from "../../assets/apricot.jpg";
 import Banana from "../../assets/banana.jpg";
 import Cherry from "../../assets/cherry.jpg";
 function Items({ item, setItem }) {
-  const notify = () => toast("Added to Cart");
-  const [product, setProducts] = useState([
-    {
-      id: 1,
-      title: "Apple",
-      price: 10.95,
-      image: `${Apple}`,
-    },
-    {
-      id: 2,
-      title: "Apricot",
-      price: 20.85,
-      image: `${Banana}`,
-    },
-    {
-      id: 3,
-      title: "Cherry",
-      price: 90.95,
-      image: `${Cherry}`,
-    },
-    {
-      id: 4,
-      title: "Apricot",
-      price: 98.99,
-      image: `${Apricot}`,
-    },
-  ]);
-  const [loading, setLoading] = useState(false);
-  // useEffect(() => {
-  //   const axios = require("axios");
-  //   axios
-  //     .get("https://fakestoreapi.com/products/")
-  //     .then(function (response) {
-  //       // handle success
-  //       setProducts(response.data);
-  //     })
-  //     .catch(function (error) {
-  //       // handle error
-  //       console.log(error);
-  //     });
-  // }, []);
-  function buy(bought) {
-    notify();
-    const newItem = [...item, bought];
-    setItem(newItem);
-  }
-  let listProducts = () => {
-    return product.map((produc) => (
-      <>
-        <Child key={produc.id}>
-          <ImageContainer>
-            <ProductImg src={produc.image} alt={produc.title} />
-          </ImageContainer>
-          <Empdiv>
-            <HeadProduct>{produc.title}</HeadProduct>
-            <PriceProduct>${produc.price}</PriceProduct>
-            <Buttoncart onClick={() => buy(produc)}>Add to Cart</Buttoncart>
-            <Links to={`cart/${produc.id}`}>View Product</Links>
-            <ToastContainer autoClose={300}/>
-          </Empdiv>
-        </Child>
-      </>
-    ));
-  };
-  return (
-    <>
-      <MainContainer>
-        <Wrapperlist>
-          <ParentList>{listProducts()}</ParentList>
-        </Wrapperlist>
-      </MainContainer>
-    </>
-  );
+    const notify = () => toast("Added to Cart");
+    const [product, setProducts] = useState([
+        {
+            id: 1,
+            title: "Apple",
+            price: 10.95,
+            image: `${Apple}`,
+        },
+        {
+            id: 2,
+            title: "Apricot",
+            price: 20.85,
+            image: `${Banana}`,
+        },
+        {
+            id: 3,
+            title: "Cherry",
+            price: 90.95,
+            image: `${Cherry}`,
+        },
+        {
+            id: 4,
+            title: "Apricot",
+            price: 98.99,
+            image: `${Apricot}`,
+        },
+    ]);
+    const [loading, setLoading] = useState(false);
+    // useEffect(() => {
+    //   const axios = require("axios");
+    //   axios
+    //     .get("https://fakestoreapi.com/products/")
+    //     .then(function (response) {
+    //       // handle success
+    //       setProducts(response.data);
+    //     })
+    //     .catch(function (error) {
+    //       // handle error
+    //       console.log(error);
+    //     });
+    // }, []);
+    function buy(bought) {
+        notify();
+        const newItem = [...item, bought];
+        setItem(newItem);
+    }
+    let listProducts = () => {
+        return product.map((produc) => (
+            <>
+                <Child key={produc.id}>
+                    <ImageContainer>
+                        <ProductImg src={produc.image} alt={produc.title} />
+                    </ImageContainer>
+                    <Empdiv>
+                        <HeadProduct>{produc.title}</HeadProduct>
+                        <PriceProduct>${produc.price}</PriceProduct>
+                        <Buttoncart onClick={() => buy(produc)}>
+                            Add to Cart
+                        </Buttoncart>
+                        <Links to={`cart/${produc.id}`}>View Product</Links>
+                        <ToastContainer autoClose={300} />
+                    </Empdiv>
+                </Child>
+            </>
+        ));
+    };
+    return (
+        <>
+            <MainContainer>
+                <Wrapperlist>
+                    <ParentList>{listProducts()}</ParentList>
+                </Wrapperlist>
+            </MainContainer>
+        </>
+    );
 }
 const Buts = styled.a`
-  cursor: pointer;
-  background: green;
-  border-radius: 10px;
+    cursor: pointer;
+    background: green;
+    border-radius: 10px;
 `;
 const Empdiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 const Wrapperlist = styled.div`
-  width: 92%;
-  margin: 0 auto;
+    width: 92%;
+    margin: 0 auto;
 `;
 const MainContainer = styled.section`
-  background: aliceblue;
+    background: aliceblue;
 `;
 const ParentList = styled.ul`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  padding-top: 97px;
-  margin-top: -1px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding-top: 97px;
+    margin-top: -1px;
 `;
 const Child = styled.li`
-  width: 23%;
-  margin-bottom: 46px;
-  border-radius: 4px;
-  padding: 4px;
-  background: #fff;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  @media (max-width: 768px) {
-    width: 46%;
-    flex-wrap: wrap;
-  }
+    width: 23%;
+    margin-bottom: 46px;
+    border-radius: 4px;
+    padding: 4px;
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    @media (max-width: 768px) {
+        width: 46%;
+        flex-wrap: wrap;
+    }
 `;
 const ImageContainer = styled.div`
-  width: 80%;
-  max-width: 68%;
-  overflow: hidden;
+    width: 80%;
+    max-width: 68%;
+    overflow: hidden;
 `;
 const ProductImg = styled.img`
-  display: block;
-  width: 100%;
-  border-radius: 15px;
+    display: block;
+    width: 100%;
+    border-radius: 15px;
 `;
 const HeadProduct = styled.h4`
-  height: 1.5rem;
-  overflow: hidden;
+    height: 1.5rem;
+    overflow: hidden;
 `;
 const PriceProduct = styled.p`
-  margin-top: -9px;
+    margin-top: -9px;
 `;
 const Buttoncart = styled.a`
-  background: #0fa675;
-  color: white;
-  border-radius: 2px;
-  padding: 4px;
-  cursor: pointer;
-  :hover {
-    color: black;
-  }
+    background: #0fa675;
+    color: white;
+    border-radius: 2px;
+    padding: 4px;
+    cursor: pointer;
+    :hover {
+        color: black;
+    }
 `;
 const Links = styled(Link)`
-  margin: 8px 0;
-  text-decoration: none;
-  display: flex;
-  background: black;
-  padding: 5px;
-  border-radius: 4px;
-  color: white;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  :hover {
+    margin: 8px 0;
+    text-decoration: none;
+    display: flex;
+    background: black;
+    padding: 5px;
+    border-radius: 4px;
     color: white;
-  }
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    :hover {
+        color: white;
+    }
 `;
 export default Items;
