4d6d70ceb9ac9bc31517c37defda4c654cca3990
nafihpp <nafihpp>
2022-09-13 21:06:12 +0530

    element page updated

--- src/Components/Screens/Elements.jsx
+++ src/Components/Screens/Elements.jsx
@@ -4,10 +4,12 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Helmet from "react-helmet";
 import styled from "styled-components";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 function Elements({ item, setItem }) {
     const { id } = useParams();
     const [page, setPage] = useState([]);
+    const [count, setCount] = useState(0);
     useEffect(() => {
         axios
             .get(`https://fakestoreapi.com/products/${id}`)
@@ -21,9 +23,13 @@ function Elements({ item, setItem }) {
             });
     }, [id]);
 
-    function buyed(boughted) {
-        const now = [...item, boughted];
+    function increment() {
+        setCount((prev) => prev + 1);
     }
+    function decrement() {
+        setCount((prev) => prev - 1);
+    }
+
     return (
         <>
             <Helmet>
@@ -36,37 +42,48 @@ function Elements({ item, setItem }) {
                             <img src={page.image} alt="" className="image" />
                         </ImageContainer>
                         <div class="right">
-                            <p class="company">{page.title}</p>
-                            <h1>{page.category}</h1>
-                            <p>{page.description}</p>
+                            <h1>{page.title}</h1>
+                            <p
+                                style={{
+                                    border: "1px solid black",
+                                    width: "40%",
+                                    fontSize: "13px",
+                                    minHeight: "30px",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    borderRadius: "8px",
+                                }}
+                            >
+                                {page.category}
+                            </p>
+                            {page.description}
                             <div class="prices">
-                                <h1 class="new">$125.00</h1>
-                                <span>50%</span>
-                                <h3 class="old">$250.00</h3>
+                                <h1 class="new">${page.price}</h1>
+                                <span>10%</span>
                             </div>
                             <div class="cart-items">
                                 <div class="left">
-                                    <a
+                                    <div
                                         href=""
                                         class="operator"
                                         style={{ textDecoration: "none" }}
+                                        onClick={() => decrement()}
                                     >
                                         -
-                                    </a>
-                                    <h5>0</h5>
-                                    <a
+                                    </div>
+                                    <h5>{count}</h5>
+                                    <div
                                         href=""
                                         class="operator"
                                         style={{ textDecoration: "none" }}
+                                        onClick={() => increment()}
                                     >
                                         +
-                                    </a>
+                                    </div>
                                 </div>
-                                <div class="right" onClick={() => buyed(page)}>
-                                    <a href="#">
-                                        <div class="cartimage"></div>
-                                        <h4 class="white">Add to cart</h4>
-                                    </a>
+                                <div class="right">
+                                    <a href="#">Add to cart</a>
                                 </div>
                             </div>
                         </div>
